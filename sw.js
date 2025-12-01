7840f68d2f1914f610d3ef8186c05f02c126510b
Guilherme22637 <sp4693068@gmail.com>
2025-12-01 11:10:52 -0300

    Refactor service worker caching logic and assets

--- sw.js
+++ sw.js
@@ -1,22 +1,18 @@
 const cacheName = "recicla-cache-v1";
-const assetsToCache = [
+const assets = [
   "/",
   "/index.html",
   "/styles.css",
   "/script.js",
   "/manifest.json",
-  "https://unpkg.com/leaflet/dist/leaflet.css",
-  "https://unpkg.com/leaflet/dist/leaflet.js"
+  "https://unpkg.com/leaflet@1.9.3/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assetsToCache))
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(cacheName).then(c => c.addAll(assets)));
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
